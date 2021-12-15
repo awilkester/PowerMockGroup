e87001995527c2b3a07a23357c227015ad7a605e
awilkester <awilkes2589@gmail.com>
2021-12-15 13:19:34 +0100

    changes for presentation

--- src/main/java/Utility.java
+++ src/main/java/Utility.java
@@ -5,4 +5,8 @@ public class Utility {
     public static String staticMethod(String message) {
         return message;
     }
+
+    public final String finalMethod(String message) {
+        return message;
+    }
 }
--- src/test/java/Powermock_test.java
+++ src/test/java/Powermock_test.java
@@ -16,32 +16,28 @@ import org.powermock.modules.junit4.PowerMockRunner;
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(Utility.class)
 @PowerMockIgnore("jdk.internal.reflect.*")
-//public class Powermock_test {
-//
-//    @Test
-//    public void TestFinalMethod_WithPowerMock() throws Exception {
-//
-//        String message = " PowerMock with Mockito and JUnit ";
-//        Utility uti = PowerMockito.mock(Utility.class);
-//        PowerMockito.whenNew(Utility.class).withNoArguments().thenReturn(uti);
-//
-//        Utility uti2 =  new Utility();
-//        PowerMockito.verifyNew(Utility.class).withNoArguments();
-//
-//        PowerMockito.when(uti2.finalMethod(message)).thenReturn(message);
-//        String message2 = uti2.finalMethod(message);
-//        Mockito.verify(uti2).finalMethod(message);
-//        assertEquals(message, message2);
-//    }
-//}
-
 public class Powermock_test {
+
+    @Test
+    public void TestFinalMethod_WithPowerMock() throws Exception {
+
+        String message = " PowerMock with Mockito and JUnit ";
+        Utility uti = PowerMockito.mock(Utility.class);
+        PowerMockito.whenNew(Utility.class).withNoArguments().thenReturn(uti);
+
+        Utility uti2 =  new Utility();
+        PowerMockito.verifyNew(Utility.class).withNoArguments();
+
+        PowerMockito.when(uti2.finalMethod(message)).thenReturn(message);
+        String message2 = uti2.finalMethod(message);
+        Mockito.verify(uti2).finalMethod(message);
+        assertEquals(message, message2);
+    }
 
     @Test
     public void TestStaticMethod_WithPowerMockito() {
 
         String call = " Hi there, I'm using PowerMock with Mockito ";
-        //String callexpectation = " Call Expectation for you. ";
 
         PowerMockito.mockStatic(Utility.class);
         PowerMockito.when(Utility.staticMethod(call)).thenReturn(" Call Expectation for you. ");
@@ -55,8 +51,6 @@ public class Powermock_test {
 powermock.module.junit4 (2.0.9)
 powermock.api.mockito2 (2.0.9)
 
-SUPPOSEDLY for testing final or static methods/classes
-
-a final
+SUPPOSEDLY for testing final, static, or private methods
 
 */
